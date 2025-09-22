3deb3ed1f7cb3d949f334027710677baba8a6b29
jaismeenk1207-ctrl <jaismeen.k1207@gmail.com>
2025-09-22 17:23:21 +0530

    Add save the trained model automatically

--- model.py
+++ model.py
@@ -9,3 +9,9 @@ def build_model():
         layers.Dense(10, activation='softmax')
     ])
     return model
+
+
+history = model.fit(x_train, y_train, epochs=2, validation_split=0.1)
+model.save('mnist_model.keras')
+from tensorflow.keras.models import load_model
+model = load_model('mnist_model.keras')
