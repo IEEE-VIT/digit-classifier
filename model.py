07daf5f1949ec7ca4db503e0e4dffb322bc32b01
TiyasDas-81 <tiyasdas81@gmail.com>
2025-09-22 17:00:49 +0530

    New

--- model.py
+++ model.py
@@ -2,10 +2,12 @@ import tensorflow as tf
 from tensorflow.keras import layers, models
 
 def build_model():
+ 
     model = models.Sequential([
-        layers.Input(shape=(28, 28)),
+        layers.InputLayer(input_shape=(28, 28)),
         layers.Flatten(),
         layers.Dense(128, activation='relu'),
+        layers.Dropout(0.2),  # Helps prevent overfitting
         layers.Dense(10, activation='softmax')
     ])
     return model
--- train.py
+++ train.py
@@ -2,11 +2,11 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 from model import build_model
 
-# Load dataset
+
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
-# Build model
+
 model = build_model()
 model.compile(optimizer="adam",
               loss="sparse_categorical_crossentropy",
@@ -15,7 +15,9 @@ model.compile(optimizer="adam",
 # Train
 history = model.fit(x_train, y_train, epochs=2, validation_split=0.1)
 
-# Evaluate
+
+model.save("digit_classifier_model.h5")  # Model will be saved after training
+
 test_loss, test_acc = model.evaluate(x_test, y_test)
 print(f"Test accuracy: {test_acc}")
 
